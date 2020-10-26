599f32fdea4e2bbd55686626283caa19f01aa92f
kseniya-mikhailiuk <mikhailiuk.k@gmail.com>
2020-10-26 20:29:41 +0300

    added fetch service with mock data

---
+++ src/services/restaurant-fetch-service.js
@@ -0,0 +1,119 @@
+const delay = (ms) =>
+    new Promise(resolve => setTimeout(resolve, ms));
+
+export const fetchRestaurantInfo = (restaurantId) =>
+    delay(500)
+        .then(() => {
+            return restaurant;
+        })
+
+
+const restaurant = {
+    "restaurant": {
+      "id": "eop1212-mkoj33-klkl44-mlml2",
+      "name": "White Rabbit",
+      "address": "Minsk, Pushkinskaya 75",
+      "siteLink": "www.whiterabbit.com",
+      "instagramLink": "www.instagram.com/whiterabbit",
+      "facebookLink": "www.fb.com/whiterabbit",
+      "logo": "mock/image.png",
+      "logoBackground": "mock/5b2ba44e15e9f916c17e7e3d 1.png",
+      "curency": "usd",
+      "timeZone": "UTC+14:00",
+      "languages": [{ id: "english", name: "english"}],
+      "menus": [
+        {
+          "name": "Dinner menu",
+          "days": "Mon, Thu, Fri",
+          "startTime": "11:00 am",
+          "endTime": "4:00 pm",
+          "sections": [
+            {
+              "name": "Seafood",
+              "dishes": [
+                {
+                  "id": "ewfewfewfewfewfewfwe",
+                  "name": "King crab, phalanx and bernoise",
+                  "description": "King crab, phalanx and bernoise super vkusno ochen nam nam",
+                  "portion": "100/50/50 gramm",
+                  "price": "10"
+                },
+                {
+                  "id": "ewfewferpmferiofmoe",
+                  "name": "Big king crab",
+                  "description": "King crab super vkusno ochen nam nam",
+                  "portion": "200 gramm",
+                  "price": "20"
+                }
+              ]
+            },
+            {
+              "name": "Salads",
+              "dishes": [
+                {
+                  "id": "ewmdoiewijgrenijgner",
+                  "name": "Ceasar",
+                  "description": "Ceasar super vkusno ochen nam nam",
+                  "portion": "200 gramm",
+                  "price": "10"
+                },
+                {
+                  "id": "wkemfoikwjenfwwekwenfoew",
+                  "name": "Pivo",
+                  "description": "Pivo super vkusno ochen nam nam",
+                  "portion": "200 gramm",
+                  "price": "100"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "Vine list",
+          "days": "",
+          "startTime": "",
+          "endTime": "",
+          "sections": [
+            {
+              "name": "Greek",
+              "dishes": [
+                {
+                  "id": "ewfewfewfewfewfewfwe",
+                  "name": "King crab, phalanx and bernoise",
+                  "description": "King crab, phalanx and bernoise super vkusno ochen nam nam",
+                  "portion": "100/50/50 gramm",
+                  "price": "10"
+                },
+                {
+                  "id": "ewfewferpmferiofmoe",
+                  "name": "Big king crab",
+                  "description": "King crab super vkusno ochen nam nam",
+                  "portion": "200 gramm",
+                  "price": "20"
+                }
+              ]
+            },
+            {
+              "name": "Belarusian",
+              "dishes": [
+                {
+                  "id": "ewmdoiewijgrenijgner",
+                  "name": "staraya kelya",
+                  "description": "Ceasar super vkusno ochen nam nam",
+                  "portion": "200 gramm",
+                  "price": "10"
+                },
+                {
+                  "id": "wkemfoikwjenfwwekwenfoew",
+                  "name": "Pivo",
+                  "description": "Pivo super vkusno ochen nam nam",
+                  "portion": "200 gramm",
+                  "price": "100"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  }
